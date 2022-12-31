221b9504ee787649515a9c2d812d29f290a78d29
fam007e <faisalmoshiur@gmail.com>
2022-12-31 01:59:05 +0100

    Changes to be committed: 	modified:   OPEN_AI_CHAT/openai.py

--- OPEN_AI_CHAT/openai.py
+++ OPEN_AI_CHAT/openai.py
@@ -1,10 +1,11 @@
+import argparse
 import requests
 
 def send_message(message):
     # Set the API endpoint and your API key
     endpoint = "https://api.openai.com/v1/images/generations"
     api_key = "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"
-#org-eO050d5HxeeaRMzrnAmcfZGL
+
     # Set the message payload
     payload = {
         "model": "text-davinci-003",
@@ -15,23 +16,36 @@ def send_message(message):
     }
 
     # Send the message to the OpenAI API
-    response = requests.post(endpoint, json=payload, headers={"Authorization": f"Bearer {api_key}"})
-    
-    # Print the JSON response from the OpenAI API
-    print(response.json())
-    
-    # Get the response from the OpenAI API
-    response_text = response.json()["data"][0]["url"]
+    try:
+        with requests.post(endpoint, json=payload, headers={"Authorization": f"Bearer {api_key}"}) as response:
+            # Get the response from the OpenAI API
+            response_text = response.json()["data"][0]["url"]
+    except requests.RequestException as e:
+        print(f"An error occurred while sending the message: {e}")
+        return
 
     return response_text
 
+def print_response(response):
+    print(response)
+
+def get_input():
+    return input("Enter a message: ")
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("message", help="The message to send to the chat feature")
+    return parser.parse_args()
+
 def main():
     # Send a message to the OpenAI chat feature
-    message = input("Enter a message: ")
+    args = parse_args()
+    message = args.message if args.message else get_input()
     response = send_message(message)
 
     # Print the response from the OpenAI chat feature
-    print(response)
+    if response:
+        print_response(response)
 
 if __name__ == "__main__":
     main()
