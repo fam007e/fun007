472d6a719c84e9533209a6438ec38193b62bf446
fam007e <faisalmoshiur@gmail.com>
2024-04-25 21:23:54 +0000

    python keywords printer added

---
+++ Python_kws/py_kws.py
@@ -0,0 +1,17 @@
+from keyword import kwlist, softkwlist
+
+def display_keywords() -> None: #1usage
+	print('Keywords:')
+	for i, kw in enumerate(kwlist, start=1):
+		print(f'{i:2}: {kw}')
+
+	print('Soft keywords:')
+	for i, skw in enumerate(softkwlist, start=1):
+		print(f'{i:2}: {skw}')
+
+def main() -> None: #1usage
+	display_keywords()
+
+
+if __name__ == '__main__':
+	main()
