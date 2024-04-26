e8068967f541c2efdc1b89c458c2ce1729c43bfe
fam007e <faisalmoshiur@gmail.com>
2024-04-26 18:25:01 +0600

    py_kws optimised

--- Python_kws/py_kws.py
+++ Python_kws/py_kws.py
@@ -1,17 +1,18 @@
 from keyword import kwlist, softkwlist
 
-def display_keywords() -> None: #1usage
-	print('Keywords:')
-	for i, kw in enumerate(kwlist, start=1):
-		print(f'{i:2}: {kw}')
+def display_keywords() -> None:
+    """Display Python keywords and soft keywords."""
+    print("Keywords:")
+    for i, kw in enumerate(kwlist, start=1):
+        print(f"{i:2}: {kw}")
 
-	print('Soft keywords:')
-	for i, skw in enumerate(softkwlist, start=1):
-		print(f'{i:2}: {skw}')
+    print("\nSoft keywords:")
+    for i, skw in enumerate(softkwlist, start=1):
+        print(f"{i:2}: {skw}")
 
-def main() -> None: #1usage
-	display_keywords()
+def main() -> None:
+    """Main function to display Python keywords."""
+    display_keywords()
 
-
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
